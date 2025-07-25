abe51a14a50bcd519d87b9583cc93994785f1fcf
Mariana Neves <mariana.alvesdor@gmail.com>
2025-07-25 00:59:25 +0000

    feat: primeira mensagem com deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home(){
-    return <h1>Teste</h1>
+    return (
+    <><h4>Se você tá lendo isso, significa que meu exercício da aula finalmente virou um site de verdade. 😎🚀<br />
+Obrigada por ser minha primeira visitante oficial</h4>
+<br/>
+<img src="https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif" alt="GIF celebrando" width="300" />
+</>
+)
 }
 
 export default Home;
